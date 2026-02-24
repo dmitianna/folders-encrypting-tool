ebcd14b75fd7b63f17ecc0c764b6a187f153ac64
dmitianna <dmitianna@bk.ru>
2026-02-24 19:47:51 +0700

    added a folder crawling method

--- filecrawler.h
+++ filecrawler.h
@@ -31,8 +31,56 @@ public:
 
     explicit FileCrawler(QObject *parent = nullptr) : QObject(parent) {}
 
+    ScanResult scanFolder(const QString &path)
+    {
+        ScanResult result;
+        result.success = false;
+
+        QFileInfo dirInfo(path);
+        if (!dirInfo.exists())
+        {
+            result.errorMessage = "Error: folder '" + path + "' does not exist";
+            return result;
+        }
+
+        if (!dirInfo.isDir())
+        {
+            result.errorMessage = "Error: '" + path + "' is not a folder";
+            return result;
+        }
+
+        if (!dirInfo.isReadable())
+        {
+            result.errorMessage = "Error: no read permission for '" + path + "'";
+            return result;
+        }
+
+        m_rootPath = QDir(path).absolutePath();
+
+        QDirIterator it(path,QDir::Files | QDir::Dirs | QDir::NoDotAndDotDot | QDir::Hidden,QDirIterator::Subdirectories);
+
+        while (it.hasNext())
+        {
+            it.next();
+            QFileInfo entry = it.fileInfo();
+
+            FileItem item;
+            item.filePath = entry.absoluteFilePath();
+            item.fileName = entry.fileName();
+            item.relativePath = QDir(m_rootPath).relativeFilePath(entry.absoluteFilePath());
+            item.size = entry.size();
+            item.isDir = entry.isDir();
+            item.isHidden = entry.isHidden();
+
+            result.items.append(item);
+        }
+
+        result.success = true;
+        return result;
+    }
+
 private:
     QString m_rootPath;
 };
 
-#endif // FILECRAWLER_H
+#endif
